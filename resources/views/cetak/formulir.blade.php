786d37759d7ec826812b240967eb61bbe2cf0650
asad <asad.sidiq2@gmail.com>
2025-11-02 13:17:12 +0700

    update formulir

--- resources/views/cetak/formulir.blade.php
+++ resources/views/cetak/formulir.blade.php
@@ -346,27 +346,27 @@
                     <td style="border: none;" class="text-right">cm</td>
                 </tr>
                 <tr class="text-left">
-                    <td style="border: none;"><span style="margin-left: 16px">Lebar</td>
+                    <td style="border: none;"><span style="margin-left: 22px">Lebar</td>
                     <td style="text-align: center; border: none;">=</td>
                     <td style="border: none;" class="text-right">cm</td>
                 </tr>
                 <tr class="text-left">
-                    <td style="border: none;"><span style="margin-left: 16px">Jarak antar Tempat duduk</td>
+                    <td style="border: none;"><span style="margin-left: 22">Jarak antar Tempat duduk</td>
                     <td style="text-align: center; border: none;">=</td>
                     <td style="border: none;" class="text-right">cm</td>
                 </tr>
                 <tr class="text-left">
-                    <td style="border: none;"><span style="margin-left: 16px">Jarak Lorong</td>
+                    <td style="border: none;"><span style="margin-left: 22px">Jarak Lorong</td>
                     <td style="text-align: center; border: none;">=</td>
                     <td style="border: none;" class="text-right">cm</td>
                 </tr>
                 <tr class="text-left">
-                    <td style="border: none;"><span style="margin-left: 16px">Lebar Tempat Duduk</td>
+                    <td style="border: none;"><span style="margin-left: 22px">Lebar Tempat Duduk</td>
                     <td style="text-align: center; border: none;">=</td>
                     <td style="border: none;" class="text-right">cm</td>
                 </tr>
                 <tr class="text-left">
-                    <td style="border: none;"><span style="margin-left: 16px">Tinggi Pijakan</td>
+                    <td style="border: none;"><span style="margin-left: 22px">Tinggi Pijakan</td>
                     <td style="text-align: center; border: none;">=</td>
                     <td style="border: none;" class="text-right">cm</td>
                 </tr>
