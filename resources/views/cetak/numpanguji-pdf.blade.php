34bc2186b12e85063205faf974630691c35ecb76
asad <asad.sidiq2@gmail.com>
2025-11-03 15:44:46 +0700

    update surat

--- resources/views/cetak/numpanguji-pdf.blade.php
+++ resources/views/cetak/numpanguji-pdf.blade.php
@@ -119,7 +119,7 @@
             <td style="width: 37%">
                 <p class="text-center">Cirebon, <span id="date"><?php echo tgl_indo($kendaraan->tglpendaftaran) ?></span></p>
                 <p>Kepada :</p>
-                <p><span>Yth. {{ $kendaraan->kepada }}</span></p>
+                <p style="margin: left:-28px">Yth. {{ $kendaraan->kepada }}</p>
                 <p>Di - </p>
                 <p style="margin-left: 22px"><span>{{ $kendaraan->tujuan }}</span></p>
             </td>
