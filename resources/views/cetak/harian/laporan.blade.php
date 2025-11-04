3144abc3d61a535f305a0c7decf816e79646261e
asad <asad.sidiq2@gmail.com>
2025-11-04 16:27:34 +0700

    update laporan

--- resources/views/cetak/harian/laporan.blade.php
+++ resources/views/cetak/harian/laporan.blade.php
@@ -198,19 +198,18 @@
     </div>
     <div class="page-break"> </div>
 
-    <div class="text-left">
+    {{-- <div class="text-left">
         <h3 style="margin:1;padding:1">Lampiran</h3>
         <h3 style="margin:1;padding:1">DATA ADMINISTRASI</h3>
         <h3 style="margin:1;padding:1">{{ $tglprint }}</h3>
-    </div>
+    </div> --}}
     <table style="width: 100%">
         <tr>
             <td style="width: 50%;vertical-align: top;">
-                
                 <table class="table table-bordered ">
                     <thead style="text-align:center;">
                         <tr>
-                            <th style="width:40%;" colspan="2">JENIS KENDARAAN</th>
+                            <th style="width:40%;text-align:center" colspan="2">JENIS KENDARAAN</th>
                             <th style="width:20%;">JUMLAH</th>
                         </tr>
                     </thead>
