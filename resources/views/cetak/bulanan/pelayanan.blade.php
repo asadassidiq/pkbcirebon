a524b522d2842d18ada5ca6874254745a54dcb47
asad <asad.sidiq2@gmail.com>
2025-11-07 17:22:15 +0700

    update laporan

--- resources/views/cetak/bulanan/pelayanan.blade.php
+++ resources/views/cetak/bulanan/pelayanan.blade.php
@@ -163,12 +163,11 @@
             $ujiulang = 0;
             $lulus = 0;
             $tidaklulus = 0;
-            dd($kendaraan);
             @endphp
             @foreach ($kendaraan as $data) 
             <tr>
                 <th scope="row">{{ $i }}</th>
-                <td>{{ $data->tgl }}</td>
+                <td>{{ $data['tgl'] }}</td>
                 <td>{{ $data->ujipertama }}</td>
                 <td>{{ $data->ujiberkala }}</td>
                 <td>{{ $data->numasuk }}</td>
