ae7dc12beafb1c666c0d89e090cbd0f646b171b9
asad <asad.sidiq2@gmail.com>
2025-11-10 10:41:40 +0700

    update laporan

--- resources/views/exports/bulanan/pelayanan.blade.php
+++ resources/views/exports/bulanan/pelayanan.blade.php
@@ -13,27 +13,84 @@
     </thead>
     <tbody>
         <tr>
-            <td rowspan="3">NO</td>
-            <td rowspan="3">TANGGAL</td>
-            <td colspan="8">JENIS PELAYANAN</td>
-            <td colspan="2">HASIL UJI</td>
-            <td rowspan="3">KET</td>
+            <th scope="col" rowspan="3">NO</th>
+            <th scope="col" rowspan="3">TANGGAL</th>
+            <th scope="col" colspan="8">JENIS PELAYANAN</th>
+            <th scope="col" colspan="2">HASIL UJI</th>
+            <th scope="col" rowspan="3">KET</th>
         </tr>
         <tr>
-            <td colspan="5">PELAYANAN ADMINISTRASI & TEKNIK</td>
-            <td colspan="2">PELAYANAN ADMINISTRASI</td>
-            <td rowspan="2">UJI ULANG</td>
-            <td rowspan="2">LULUS</td>
-            <td rowspan="2">TIDAK LULUS</td>
+            <th colspan="5">PELAYANAN ADMINISTRASI & TEKNIK</th>
+            <th colspan="2">PELAYANAN ADMINISTRASI</th>
+            <th rowspan="2">UJI ULANG</th>
+            <th rowspan="2">LULUS</th>
+            <th rowspan="2">TIDAK LULUS</th>
         </tr>
         <tr>
-            <td>UJI PERTAMA</td>
-            <td>UJI BERKALA</td>
-            <td>NUMPANG UJI MASUK</td>
-            <td>MUTASI MASUK</td>
-            <td>RUBAH BENTUK</td>
-            <td>NUMPANG UJI KELUAR</td>
-            <td>MUTASI KELUAR</td>
+            <th>UJI PERTAMA</th>
+            <th>UJI BERKALA</th>
+            <th>NUMPANG UJI MASUK</th>
+            <th>MUTASI MASUK</th>
+            <th>RUBAH BENTUK</th>
+            <th>NUMPANG UJI KELUAR</th>
+            <th>MUTASI KELUAR</th>
+        </tr>
+        @php
+        $i=1;
+        $ujipertama = 0;
+        $ujiberkala = 0;
+        $numasuk = 0;
+        $mutasimasuk = 0;
+        $rubahbentuk = 0;
+        $nukeluar = 0;
+        $mutasikeluar = 0;
+        $ujiulang = 0;
+        $lulus = 0;
+        $tidaklulus = 0;
+        @endphp
+        @foreach ($data as $data)
+        <tr>
+            <td scope="row">{{ $i }}</td>
+            <td>{{ $data['tgl'] }}</td>
+            <td>{{ $data['ujipertama'] }}</td>
+            <td>{{ $data['ujiberkala'] }}</td>
+            <td>{{ $data['numasuk'] }}</td>
+            <td>{{ $data['mutasimasuk'] }}</td>
+            <td>{{ $data['rubahbentuk'] }}</td>
+            <td>{{ $data['nukeluar'] }}</td>
+            <td>{{ $data['mutasikeluar'] }}</td>
+            <td>{{ $data['ujiulang'] }}</td>
+            <td>{{ $data['lulus'] }}</td>
+            <td>{{ $data['tidaklulus'] }}</td>
+            <td></td>
+        </tr>
+        @php
+        $i++;
+        $ujipertama += $data['ujipertama'];
+        $ujiberkala += $data['ujiberkala'];
+        $numasuk += $data['numasuk'];
+        $mutasimasuk += $data['mutasimasuk'];
+        $rubahbentuk += $data['rubahbentuk'];
+        $nukeluar += $data['nukeluar'];
+        $mutasikeluar += $data['mutasikeluar'];
+        $ujiulang += $data['ujiulang'];
+        $lulus += $data['lulus'];
+        $tidaklulus += $data['tidaklulus'];
+        @endphp
+        @endforeach
+        <tr>
+            <td scope="row" colspan="2">TOTAL</td>
+            <td>{{ $ujipertama }}</td>
+            <td>{{ $ujiberkala }}</td>
+            <td>{{ $numasuk }}</td>
+            <td>{{ $mutasimasuk }}</td>
+            <td>{{ $rubahbentuk }}</td>
+            <td>{{ $nukeluar }}</td>
+            <td>{{ $mutasikeluar }}</td>
+            <td>{{ $ujiulang }}</td>
+            <td>{{ $lulus }}</td>
+            <td>{{ $tidaklulus }}</td>
+            <td></td>
         </tr>
     </tbody>
 </table>
