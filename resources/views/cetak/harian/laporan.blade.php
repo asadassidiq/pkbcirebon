de98866152c579909ec4121b990179b0d9238360
asad <asad.sidiq2@gmail.com>
2025-11-20 09:58:13 +0700

    persuratan

--- resources/views/cetak/harian/laporan.blade.php
+++ resources/views/cetak/harian/laporan.blade.php
@@ -201,91 +201,93 @@
     <table style="width: 100%">
         <tr>
             <td style="width: 47%;vertical-align: top">
+                <div style="transform: scale(0.80); transform-origin: top left;">
                 <table class="table table-bordered ">
-                <thead style="text-align:center;">
-                    <tr>
-                        <th style="width:40%;text-align:center" colspan="2">JENIS KENDARAAN</th>
-                        <th style="width:20%;">JUMLAH</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <!-- MOBIL PNP -->
-                    <tr>
-                        <td rowspan="2">MOBIL PNP</td>
-                        <td>MOBIL PNP SEDAN</td>
-                        <td>{{ $totaljenis['mobil1'] ?? 0 }}</td>
-                    </tr>
-                    <tr>
-                        <td>MOBIL PNP BUKAN SEDAN</td>
-                        <td>{{ $totaljenis['mobil2'] ?? 0 }}</td>
-                    </tr>
+                    <thead style="text-align:center;">
+                        <tr>
+                            <th style="width:40%;text-align:center" colspan="2">JENIS KENDARAAN</th>
+                            <th style="width:20%;">JUMLAH</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <!-- MOBIL PNP -->
+                        <tr>
+                            <td rowspan="2">MOBIL PNP</td>
+                            <td>MOBIL PNP SEDAN</td>
+                            <td>{{ $totaljenis['mobil1'] ?? 0 }}</td>
+                        </tr>
+                        <tr>
+                            <td>MOBIL PNP BUKAN SEDAN</td>
+                            <td>{{ $totaljenis['mobil2'] ?? 0 }}</td>
+                        </tr>
 
-                    <!-- MOBIL BUS -->
-                    <tr>
-                        <td rowspan="7">MOBIL BUS</td>
-                        <td>B.KECIL</td><td>{{ $totaljenis['mobil3'] ?? 0 }}</td>
-                    </tr>
-                    <tr><td>B.SEDANG</td><td>{{ $totaljenis['mobil4'] ?? 0 }}</td></tr>
-                    <tr><td>B.BESAR</td><td>{{ $totaljenis['mobil5'] ?? 0 }}</td></tr>
-                    <tr><td>B.MAXI</td><td>{{ $totaljenis['mobil6'] ?? 0 }}</td></tr>
-                    <tr><td>B.GANDENG</td><td>{{ $totaljenis['mobil7'] ?? 0 }}</td></tr>
-                    <tr><td>B.TEMPEL</td><td>{{ $totaljenis['mobil8'] ?? 0 }}</td></tr>
-                    <tr><td>B.TINGKAT</td><td>{{ $totaljenis['mobil9'] ?? 0 }}</td></tr>
+                        <!-- MOBIL BUS -->
+                        <tr>
+                            <td rowspan="7">MOBIL BUS</td>
+                            <td>B.KECIL</td><td>{{ $totaljenis['mobil3'] ?? 0 }}</td>
+                        </tr>
+                        <tr><td>B.SEDANG</td><td>{{ $totaljenis['mobil4'] ?? 0 }}</td></tr>
+                        <tr><td>B.BESAR</td><td>{{ $totaljenis['mobil5'] ?? 0 }}</td></tr>
+                        <tr><td>B.MAXI</td><td>{{ $totaljenis['mobil6'] ?? 0 }}</td></tr>
+                        <tr><td>B.GANDENG</td><td>{{ $totaljenis['mobil7'] ?? 0 }}</td></tr>
+                        <tr><td>B.TEMPEL</td><td>{{ $totaljenis['mobil8'] ?? 0 }}</td></tr>
+                        <tr><td>B.TINGKAT</td><td>{{ $totaljenis['mobil9'] ?? 0 }}</td></tr>
 
-                    <!-- MOBIL BARANG BAK TERBUKA -->
-                    <tr>
-                        <td rowspan="6">MOBIL BARANG BAK TERBUKA</td>
-                        <td>P.UP</td><td>{{ $totaljenis['mobil10'] ?? 0 }}</td>
-                    </tr>
-                    <tr><td>DOUBLE CABIN</td><td>{{ $totaljenis['mobil11'] ?? 0 }}</td></tr>
-                    <tr><td>L.TRUCK</td><td>{{ $totaljenis['mobil12'] ?? 0 }}</td></tr>
-                    <tr><td>DUMP TRUCK</td><td>{{ $totaljenis['mobil13'] ?? 0 }}</td></tr>
-                    <tr><td>LOST BAK</td><td>{{ $totaljenis['mobil14'] ?? 0 }}</td></tr>
-                    <tr><td>CAR CARRIER</td><td>{{ $totaljenis['mobil15'] ?? 0 }}</td></tr>
+                        <!-- MOBIL BARANG BAK TERBUKA -->
+                        <tr>
+                            <td rowspan="6">MOBIL BARANG BAK TERBUKA</td>
+                            <td>P.UP</td><td>{{ $totaljenis['mobil10'] ?? 0 }}</td>
+                        </tr>
+                        <tr><td>DOUBLE CABIN</td><td>{{ $totaljenis['mobil11'] ?? 0 }}</td></tr>
+                        <tr><td>L.TRUCK</td><td>{{ $totaljenis['mobil12'] ?? 0 }}</td></tr>
+                        <tr><td>DUMP TRUCK</td><td>{{ $totaljenis['mobil13'] ?? 0 }}</td></tr>
+                        <tr><td>LOST BAK</td><td>{{ $totaljenis['mobil14'] ?? 0 }}</td></tr>
+                        <tr><td>CAR CARRIER</td><td>{{ $totaljenis['mobil15'] ?? 0 }}</td></tr>
 
-                    <!-- MOBIL BARANG BAK TERTUTUP -->
-                    <tr>
-                        <td rowspan="5">MOBIL BARANG BAK TERTUTUP</td>
-                        <td>P.UP BOX</td><td>{{ $totaljenis['mobil16'] ?? 0 }}</td>
-                    </tr>
-                    <tr><td>P.UP RANGKA</td><td>{{ $totaljenis['mobil17'] ?? 0 }}</td></tr>
-                    <tr><td>L.TRUCK BOX</td><td>{{ $totaljenis['mobil18'] ?? 0 }}</td></tr>
-                    <tr><td>BLIND VAN</td><td>{{ $totaljenis['mobil19'] ?? 0 }}</td></tr>
-                    <tr><td>DELIVERY VAN</td><td>{{ $totaljenis['mobil20'] ?? 0 }}</td></tr>
+                        <!-- MOBIL BARANG BAK TERTUTUP -->
+                        <tr>
+                            <td rowspan="5">MOBIL BARANG BAK TERTUTUP</td>
+                            <td>P.UP BOX</td><td>{{ $totaljenis['mobil16'] ?? 0 }}</td>
+                        </tr>
+                        <tr><td>P.UP RANGKA</td><td>{{ $totaljenis['mobil17'] ?? 0 }}</td></tr>
+                        <tr><td>L.TRUCK BOX</td><td>{{ $totaljenis['mobil18'] ?? 0 }}</td></tr>
+                        <tr><td>BLIND VAN</td><td>{{ $totaljenis['mobil19'] ?? 0 }}</td></tr>
+                        <tr><td>DELIVERY VAN</td><td>{{ $totaljenis['mobil20'] ?? 0 }}</td></tr>
 
-                    <!-- MOBIL TANGKI -->
-                    <tr><td>MOBIL TANGKI</td><td></td><td>{{ $totaljenis['mobil21'] ?? 0 }}</td></tr>
+                        <!-- MOBIL TANGKI -->
+                        <tr><td>MOBIL TANGKI</td><td></td><td>{{ $totaljenis['mobil21'] ?? 0 }}</td></tr>
 
-                    <!-- MOBIL PENARIK -->
-                    <tr><td>MOBIL PENARIK</td><td></td><td>{{ $totaljenis['mobil22'] ?? 0 }}</td></tr>
+                        <!-- MOBIL PENARIK -->
+                        <tr><td>MOBIL PENARIK</td><td></td><td>{{ $totaljenis['mobil22'] ?? 0 }}</td></tr>
 
-                    <!-- KERETA -->
-                    <tr><td>KERETA GANDENG BAK TERBUKA</td><td></td><td>{{ $totaljenis['mobil23'] ?? 0 }}</td></tr>
-                    <tr><td>KERETA GANDENG BAK TERTUTUP</td><td></td><td>{{ $totaljenis['mobil24'] ?? 0 }}</td></tr>
-                    <tr><td>KERETA GANDENG TANGKI</td><td></td><td>{{ $totaljenis['mobil25'] ?? 0 }}</td></tr>
-                    <tr><td>KERETA TEMPELAN BAK TERBUKA</td><td></td><td>{{ $totaljenis['mobil26'] ?? 0 }}</td></tr>
-                    <tr><td>KERETA TEMPELAN BAK TERTUTUP</td><td></td><td>{{ $totaljenis['mobil27'] ?? 0 }}</td></tr>
-                    <tr><td>KERETA TEMPELAN TANGKI</td><td></td><td>{{ $totaljenis['mobil28'] ?? 0 }}</td></tr>
+                        <!-- KERETA -->
+                        <tr><td>KERETA GANDENG BAK TERBUKA</td><td></td><td>{{ $totaljenis['mobil23'] ?? 0 }}</td></tr>
+                        <tr><td>KERETA GANDENG BAK TERTUTUP</td><td></td><td>{{ $totaljenis['mobil24'] ?? 0 }}</td></tr>
+                        <tr><td>KERETA GANDENG TANGKI</td><td></td><td>{{ $totaljenis['mobil25'] ?? 0 }}</td></tr>
+                        <tr><td>KERETA TEMPELAN BAK TERBUKA</td><td></td><td>{{ $totaljenis['mobil26'] ?? 0 }}</td></tr>
+                        <tr><td>KERETA TEMPELAN BAK TERTUTUP</td><td></td><td>{{ $totaljenis['mobil27'] ?? 0 }}</td></tr>
+                        <tr><td>KERETA TEMPELAN TANGKI</td><td></td><td>{{ $totaljenis['mobil28'] ?? 0 }}</td></tr>
 
-                    <!-- KB RODA TIGA -->
-                    <tr><td>KB Roda Tiga Angkutan Barang Bak Muatan Terbuka</td><td></td><td>{{ $totaljenis['mobil29'] ?? 0 }}</td></tr>
-                    <tr><td>KB Roda Tiga Angkutan Barang Bak Muatan Tertutup</td><td></td><td>{{ $totaljenis['mobil30'] ?? 0 }}</td></tr>
-                    <tr><td>KB Roda Tiga Angkutan Penumpang</td><td></td><td>{{ $totaljenis['mobil31'] ?? 0 }}</td></tr>
-                    <tr><td>KB Roda Tiga Angkutan Barang Tangki</td><td></td><td>{{ $totaljenis['mobil32'] ?? 0 }}</td></tr>
+                        <!-- KB RODA TIGA -->
+                        <tr><td>KB Roda Tiga Angkutan Barang Bak Muatan Terbuka</td><td></td><td>{{ $totaljenis['mobil29'] ?? 0 }}</td></tr>
+                        <tr><td>KB Roda Tiga Angkutan Barang Bak Muatan Tertutup</td><td></td><td>{{ $totaljenis['mobil30'] ?? 0 }}</td></tr>
+                        <tr><td>KB Roda Tiga Angkutan Penumpang</td><td></td><td>{{ $totaljenis['mobil31'] ?? 0 }}</td></tr>
+                        <tr><td>KB Roda Tiga Angkutan Barang Tangki</td><td></td><td>{{ $totaljenis['mobil32'] ?? 0 }}</td></tr>
 
-                    <!-- KENDARAAN KHUSUS -->
-                    <tr>
-                        <td rowspan="7">KENDARAAN KHUSUS</td>
-                        <td>AMBULANCE</td><td>{{ $totaljenis['mobil33'] ?? 0 }}</td>
-                    </tr>
-                    <tr><td>DAMKAR</td><td>{{ $totaljenis['mobil34'] ?? 0 }}</td></tr>
-                    <tr><td>ARM ROLL</td><td>{{ $totaljenis['mobil35'] ?? 0 }}</td></tr>
-                    <tr><td>DEREK</td><td>{{ $totaljenis['mobil36'] ?? 0 }}</td></tr>
-                    <tr><td>FLAT DECK</td><td>{{ $totaljenis['mobil37'] ?? 0 }}</td></tr>
-                    <tr><td>MIXER</td><td>{{ $totaljenis['mobil38'] ?? 0 }}</td></tr>
-                    <tr><td>CONCREEDPUMP</td><td>{{ $totaljenis['mobil39'] ?? 0 }}</td></tr>
-                </tbody>
-            </table>
+                        <!-- KENDARAAN KHUSUS -->
+                        <tr>
+                            <td rowspan="7">KENDARAAN KHUSUS</td>
+                            <td>AMBULANCE</td><td>{{ $totaljenis['mobil33'] ?? 0 }}</td>
+                        </tr>
+                        <tr><td>DAMKAR</td><td>{{ $totaljenis['mobil34'] ?? 0 }}</td></tr>
+                        <tr><td>ARM ROLL</td><td>{{ $totaljenis['mobil35'] ?? 0 }}</td></tr>
+                        <tr><td>DEREK</td><td>{{ $totaljenis['mobil36'] ?? 0 }}</td></tr>
+                        <tr><td>FLAT DECK</td><td>{{ $totaljenis['mobil37'] ?? 0 }}</td></tr>
+                        <tr><td>MIXER</td><td>{{ $totaljenis['mobil38'] ?? 0 }}</td></tr>
+                        <tr><td>CONCREEDPUMP</td><td>{{ $totaljenis['mobil39'] ?? 0 }}</td></tr>
+                    </tbody>
+                </table>
+                </div>
             </td>
             <td style="width: 3%;vertical-align: top"></td>
             <td style="width: 45%;vertical-align: top">
