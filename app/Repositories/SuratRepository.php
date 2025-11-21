c7e0bbf69f92e8c2282d13631644efdcecb6116a
asad <asad.sidiq2@gmail.com>
2025-11-21 18:48:22 +0700

    pendaftran

--- app/Repositories/SuratRepository.php
+++ app/Repositories/SuratRepository.php
@@ -204,19 +204,19 @@ class SuratRepository
 
     public function createSurat($request)
     {
-        $data = Persuratan::create([
-                'pendaftaran_id' => $request['pendaftaran_id'],
-                'nosurat' => $request['nosurat'],
-                'nosuratdari' => $request['nosuratdari'],
-                'kode' => $request['kode'],
-                'kepada' => $request['kepada'],
-                'tujuan' => $request['tujuan'],
-                'alasan' => $request['alasan'],
-                'tglpelaksanaan' => $request['tglpelaksanaan'],
-                'nokendaraanbaru' => $request['nokendaraanbaru'],
-                'namapemilikbaru' => $request['namapemilikbaru'],
-                'alamatpemilikbaru	' => $request['alamatpemilikbaru'],
-            ]);
+        $data = $this->modelS::create([
+            'pendaftaran_id'   => $request['pendaftaran_id'],
+            'nosurat'          => $request['nosurat'],
+            'nosuratdari'      => $request['nosuratdari'],
+            'kode'             => $request['kode'],
+            'kepada'           => $request['kepada'],
+            'tujuan'           => $request['tujuan'],
+            'alasan'           => $request['alasan'],
+            'tglpelaksanaan'   => $request['tglpelaksanaan'],
+            'nokendaraanbaru'  => $request['nokendaraanbaru'],
+            'namapemilikbaru'  => $request['namapemilikbaru'],
+            'alamatpemilikbaru'=> $request['alamatpemilikbaru'],
+        ]);
         return $data;
     }
 
