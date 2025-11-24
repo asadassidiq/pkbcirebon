54e018eacee0494c2447fe6d98aa0b7e41edf19f
asad <asad.sidiq2@gmail.com>
2025-11-24 21:19:55 +0700

    update laporan

--- resources/views/cetak/triwulan/kartu.blade.php
+++ resources/views/cetak/triwulan/kartu.blade.php
@@ -147,7 +147,7 @@
             $rusak=0;
             $hilang=0;
             @endphp
-            @foreach ($data as $data) 
+            @foreach ($kendaraan as $data) 
             <tr>
                 <th scope="row">{{ $i }}</th>
                 <td>{{ $data['bulan'] }}</td>
