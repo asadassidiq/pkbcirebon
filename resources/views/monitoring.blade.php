0d5b0f8cc9a2fa846dfde5bc760c25fe5ea7a25f
asad <asad.sidiq2@gmail.com>
2025-11-26 00:01:50 +0700

    update pos

--- resources/views/monitoring.blade.php
+++ resources/views/monitoring.blade.php
@@ -32,9 +32,18 @@
                         
                         <!--begin::Body-->
                         <div class="card-body text-center">
-                            <div style="text-align:center;font-size: 32px;font-weight: bold;">
-                                <p>UPTD Pengujian Kendaraan Bermotor <br> Dinas Perhubungan Kota Cirebon</p>
-                            </div> 
+							<div class="row">
+								<div class="col-4">
+									<img width="50%" height="50%" src="{{ asset('img/kota.png') }}" class="img-fluid">
+								</div>
+								<div class="col-4">
+									<div style="text-align:center;font-size: 32px;font-weight: bold;">
+										<p>UPTD Pengujian Kendaraan Bermotor <br> Dinas Perhubungan Kota Cirebon</p>
+									</div> 
+								</div>
+								<div class="col-4">
+								</div>
+							</div>
                         </div>
                         <!--end::Body-->
                     </div>
