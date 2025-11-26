95d230ed66823098180d34b7d95e0f73147b8bc6
asad <asad.sidiq2@gmail.com>
2025-11-26 13:51:17 +0700

    update antrian

--- resources/js/view/pages/monitoring/Table.vue
+++ resources/js/view/pages/monitoring/Table.vue
@@ -59,7 +59,7 @@
                     <small class="text-danger d-block">{{ monitoring.nama_user_pos1 }}</small>
                   </a>
                 </td>
-                <td v-else-if="monitoring.pos1 == 1" class="text-center">å
+                <td v-else-if="monitoring.pos1 == 1" class="text-center">
                   <a href="javascript:void(0)" class="d-block">
                     <i class="flaticon-correct text-success"></i>
                     <small class="text-danger d-block">{{ monitoring.nama_user_pos1 }}</small>
