7eeeff1538f7f0f58aba98dfd9f283667ec6d264
asad <asad.sidiq2@gmail.com>
2025-11-26 13:31:31 +0700

    update antrian

--- resources/js/core/services/store/pendaftaran.module.js
+++ resources/js/core/services/store/pendaftaran.module.js
@@ -1658,6 +1658,7 @@ export const actions = {
             PendaftaranService.getLastAntrian(state.pendaftaran.tglpendaftaran)
                 .then(response => {
                     resolve(response.data);
+                    console.log(response.data);
                     state.pendaftaran.noantrian = response.data.result;
                 })
                 .catch(error => {
